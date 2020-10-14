928ef7a33f390589b90aff4057e4afb3f6c41053
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-10-14 22:56:09 +0800

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
-
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <td>DevOps</td>
+        <td>1</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td>Enterprise Web Dev</td>
+        <td>3</td>
+        <td>3</td>
+      </tr>
+    </tbody >
+  </table>
   );
 };
 
